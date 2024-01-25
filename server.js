322586d5fe9910b2f1667cd27171697dc5025313
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-01-25 17:09:54 -0500

    added mysql connection

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+
+//Create connection to our mysql database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3001',
+  user: 'root',
+  password: '',
+  database: 'employeeTracker_db',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the database!");
+  start();
+});
