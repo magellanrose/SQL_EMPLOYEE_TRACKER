3fe2533abc1492f2315f6f59202eabebb7fa6649
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-01-26 16:42:55 -0500

    updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,24 @@
 DROP DATABASE IF EXISTS employeeTracker_db;
 
 CREATE DATABASE employeeTracker_db;
+
+
+CREATE TABLE departments (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL(10,2),
+department_id INT
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT NOT NULL
+);
